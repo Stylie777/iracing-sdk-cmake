51bbe1a15db18eb469443b9b04569c41411cb57e
Jack Styles <99514724+Stylie777@users.noreply.github.com>
2023-11-13 21:55:10 +0000

    cmake: Add Windows Toolchain File

---
+++ toolchain/mingw-w64-windows.cmake
@@ -0,0 +1,12 @@
+# Copyright Jack Styles (stylie777) 2023
+# License: 
+
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Windows)
+
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc.exe)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++.exe)
+set(CMAKE_RC_COMPILER windres.exe)
+set(CMAKE_AR x86_64-w64-mingw32-gcc-ar.exe)
+set(CMAKE_RANLIB x86_64-w64-mingw32-gcc-ranlib.exe)
